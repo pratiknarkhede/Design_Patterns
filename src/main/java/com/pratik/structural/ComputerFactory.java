1c63be92deb13859514ad209442c9a1eebee84fb
Pratik Narkhede <pratiknarkhede646@gmail.com>
2022-10-19 11:47:00 +0530

    Added Factory Design Pattern Example

---
+++ src/main/java/com/pratik/structural/ComputerFactory.java
@@ -0,0 +1,15 @@
+package com.pratik.structural;
+
+public class ComputerFactory {
+	/* based on the input parameter, different subclass is created and returned */
+	
+public static Computer getComputer(String type, String ram, String hdd, String cpu) {
+	
+	if("PC".equalsIgnoreCase(type)) {
+		return new PC(ram, hdd, cpu);
+	}else if("Server".equalsIgnoreCase(type)) {
+		return new Server(ram, hdd, cpu);
+	}
+	return null;	
+}
+}
